b7a271843703dc0e70741f895edf53bbd5a00645
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2025-05-20 02:14:12 +0530

    feat: add file retrieval and management features including getSingleFile, getStarredFiles, and getRecentUploads; refactor routes for better organization

--- backend/routes/folder.routes.js
+++ backend/routes/folder.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createFolder,
   moveFileToFolder,
+  bulkMoveFilesToFolder,
   getFilesInFolder,
   getAllFoldersForUser,
 } from "../controllers/file.controller.js";
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.post("/create-folder", authMiddleware, createFolder);
 router.post("/move-file-to-folder", authMiddleware, moveFileToFolder);
+router.post("/bulk-move-files", authMiddleware, bulkMoveFilesToFolder);
 router.get("/get-files", authMiddleware, getFilesInFolder);
 router.get("/get-all-folders", authMiddleware, getAllFoldersForUser);
 
